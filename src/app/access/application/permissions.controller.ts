4f52baaec6e02cf39864562dad41e94953250b2b
Elmer Entenza <eentenza@MacBook-Pro.local>
2024-02-12 17:55:55 -0600

    resources and permissions

---
+++ src/app/access/application/permissions.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from "@nestjs/common";
+import { jsonPermissions } from "../domain/permissions";
+
+@Controller('permissions')
+export class PermissionsController {
+    @Get()
+    permissions() {
+        return jsonPermissions;
+    }
+}
