957d1071c01b7bd87d3f00346e03ec90a8ce4cf4
Elmer Entenza <plataformacu2024@gmail.com>
2024-02-28 15:34:38 -0600

    update country param id

--- src/app/country/application/update-country/update-country.command.ts
+++ src/app/country/application/update-country/update-country.command.ts
@@ -1,17 +1,14 @@
 import { Injectable } from "@nestjs/common";
-import { ID } from "../../../shared/abstract-repository/repository.interface";
 import { Continent } from "../../domain/country.interface";
 import { UpdateCountryRequest } from "./update-country.request";
 
 @Injectable()
 export class UpdateCountryCommand {
-  id: ID;
   code: string;
   name: string;
   continent: Continent;
   constructor(request: UpdateCountryRequest) {
-    const { id, code, name, continent } = request;
-    this.id = id;
+    const { code, name, continent } = request;
     this.code = code;
     this.name = name;
     this.continent = continent;
--- src/app/country/application/update-country/update-country.usecase.ts
+++ src/app/country/application/update-country/update-country.usecase.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { CountryService } from "../../domain/country.service";
 import { UpdateCountryCommand } from "./update-country.command";
+import { ID } from "src/app/shared/abstract-repository/repository.interface";
 
 @Injectable()
 export class UpdateCountryUseCse {
@@ -9,7 +10,7 @@ export class UpdateCountryUseCse {
     private readonly countryService: CountryService,
   ) {}
 
-  async execute(request: UpdateCountryCommand) {
-    return await this.countryService.updateCountry(request);
+  async execute(id: ID, request: UpdateCountryCommand) {
+    return await this.countryService.updateCountry({ id, ...request });
   }
 }
