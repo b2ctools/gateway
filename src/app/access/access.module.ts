aa0468ae6e7a43f87bc5354b208e966e74d5842d
Elmer Entenza <plataformacu2024@gmail.com>
2024-02-28 17:20:16 -0600

    link account to permissions module

--- src/app/access/access.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import { PermissionsController } from "./application/permissions.controller";
-
-@Module({
-  controllers: [PermissionsController],
-})
-export class AccessModule {}
--- src/app/account/application/set-permissions/set-permissions.usecase.ts
+++ src/app/account/application/set-permissions/set-permissions.usecase.ts
@@ -1,29 +1,32 @@
-import { BadRequestException, Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { AccountService } from "../../domain/account.service";
 import { SetPermissionsRequest } from "./set-permissions.request";
 import { ID } from "../../../shared/abstract-repository/repository.interface";
-import { isValidPermission } from "../../../access/domain/permissions";
+import { PermissionService } from "src/app/permission/domain/permission.service";
 
 @Injectable()
 export class SetPermissionsUseCase {
   constructor(
     @Inject(AccountService)
     private readonly accountService: AccountService,
+
+    @Inject(PermissionService)
+    private readonly permissionService: PermissionService,
   ) {}
 
-  private validatePermissions(permissions: ID[]) {
-    permissions.forEach((permission: ID) => {
-      if (!isValidPermission(permission)) {
-        throw new BadRequestException(
-          `Permission with id [${permission}] is no valid.`,
-        );
-      }
-    });
+  private async validatePermissions(permissions: ID[]) {
+    if (permissions && permissions.length > 0) {
+      await Promise.all(
+        permissions.map((permission) => {
+          this.permissionService.findByIdOrFail(permission);
+        })
+      )
+    }
   }
 
   async execute(id: ID, request: SetPermissionsRequest) {
     const { permissions } = request;
-    this.validatePermissions(permissions);
+    await this.validatePermissions(permissions);
     return await this.accountService.setPermissions(id, permissions);
   }
 }
