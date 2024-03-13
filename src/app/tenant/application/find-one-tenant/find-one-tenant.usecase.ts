bf15c51a3a357e9768f8135a6c1663961c8a0eb9
Elmer Entenza <plataformacu2024@gmail.com>
2024-03-13 11:47:39 -0600

    fix update tenant

--- src/app/tenant/application/find-one-tenant/find-one-tenant.usecase.ts
+++ src/app/tenant/application/find-one-tenant/find-one-tenant.usecase.ts
@@ -2,16 +2,30 @@ import { Inject, Injectable } from "@nestjs/common";
 import { ID } from "../../../shared/abstract-repository/repository.interface";
 import { TenantDto, tenantToDto } from "../../domain/tenant.interface";
 import { TenantService } from "../../domain/tenant.service";
+import { UserService } from "src/app/user/domain/user.service";
 
 @Injectable()
 export class FineOneTenantUseCase {
   constructor(
     @Inject(TenantService)
     private readonly tenantService: TenantService,
+
+    @Inject(UserService)
+    private readonly userService: UserService,    
   ) {}
 
+  private async getPrimaryOwner(userId: ID){
+    try {
+      return await this.userService.findByIdOrFail(userId);
+    } catch (error) {
+      return undefined;
+    }
+  }
   async execute(id: ID): Promise<TenantDto> {
     const tenant = await this.tenantService.findByIdOrFail(id);
+    const owner = await this.getPrimaryOwner(tenant.primaryOwnerId);
+    return tenantToDto(tenant, undefined, owner);
+
     return tenantToDto(tenant);
   }
 }
--- src/app/tenant/application/update-tenant/update-tenant.controller.ts
+++ src/app/tenant/application/update-tenant/update-tenant.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Inject, Param, Patch } from "@nestjs/common";
 import { tenantPath } from "../../../shared/routes";
-import { tenantToDto } from "../../domain/tenant.interface";
+import { TenantDto } from "../../domain/tenant.interface";
 import { UpdateTenantUseCse } from "./update-tenant.usecase";
 import { UpdateTenantRequest } from "./update-tenant.request";
 import { ID } from "src/app/shared/abstract-repository/repository.interface";
@@ -16,8 +16,8 @@ export class UpdateTenantController {
   async updateTenant(
     @Param("id") id: ID,
     @Body() request: UpdateTenantRequest,
-  ) {
-    const pc = await this.useCase.execute(id, request);
-    return tenantToDto(pc);
+  ): Promise<TenantDto> {
+    return await this.useCase.execute(id, request);
+
   }
 }
